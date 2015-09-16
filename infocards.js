7060dfebf949b577b6d71b9fcb1383f48e8de8c3
DelrithInfinity <github@kyleherndon.com>
2015-09-16 11:26:00 -0500

    Added functionality
    Cards are now produced dynamically

---
+++ infocards.js
@@ -0,0 +1,30 @@
+var infocards = [
+{
+    title:"CS 233",
+    subtitle:"Computer Architecture",
+    infopage:"I am currently taking this class",
+    page:1
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+]
